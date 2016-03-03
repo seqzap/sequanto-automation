5627fda988333c64091cfaefc800277c9196577e
Rasmus Toftdahl Olesen <github@halfdans.net>
2016-03-03 21:54:50 +0100

    Fix Arduino Due Can stream (also has some debugging output at the moment)

--- server/c/src/sq_stream_arduino_due_can.c
+++ server/c/src/sq_stream_arduino_due_can.c
@@ -31,15 +31,37 @@ typedef struct _SQStream
 } _SQStream;
 
 static CAN_FRAME s_sendFrame;
+static SQByte s_sendIndex;
+static CAN_FRAME s_frame;
+static SQByte s_dataIndex;
 
 SQStream * sq_stream_open ( int _portNumber )
 {
    Can0.init(SQ_ARDUINO_CAN_BAUD_RATE);
+   Can0.watchFor();
+
+   /*
+   int filter;
+   // extended
+   for (filter = 0; filter < 3; filter++)
+   {
+     Can0.setRXFilter(filter, 0, 0, true);
+   }
+   //standard
+   for (filter = 3; filter < 7; filter++)
+   {
+     Can0.setRXFilter(filter, 0, 0, false);
+     }*/
+
+   s_dataIndex = 0;
+   s_frame.length = 0;
    s_sendFrame.id = SQ_ARDUINO_CAN_ID;
-    SQStream * ret = (SQStream*) malloc ( sizeof(SQStream) );
-    ret->m_handler = NULL;
-    ret->m_handlerData = NULL;
-    return ret;
+   s_sendFrame.priority = 0;
+   s_sendFrame.extended = true;
+   SQStream * ret = (SQStream*) malloc ( sizeof(SQStream) );
+   ret->m_handler = NULL;
+   ret->m_handlerData = NULL;
+   return ret;
 }
 
 void sq_stream_close ( SQStream * _stream )
@@ -57,30 +79,71 @@ void sq_stream_poll( SQStream * _stream )
 {
 }
 
-static CAN_FRAME s_frame;
-static SQByte s_dataIndex;
 size_t sq_stream_data_available ( SQStream * _stream )
 {
-   if (Can0.rx_avail())
+   if ( s_dataIndex == s_frame.length )
    {
-      Can0.read(s_frame);
-      s_dataIndex = 0;
-      return s_frame.length;
+      if (Can0.rx_avail())
+      {
+         Serial.println(" RX");
+         Can0.read(s_frame);
+         s_dataIndex = 0;
+         return s_frame.length;
+      }
+      else
+      {
+         return 0;
+      }
    }
    else
    {
-      return 0;
+      return s_frame.length - s_dataIndex;
    }
+}
+
+SQBool sq_stream_read_byte ( SQStream * _stream, SQByte * _byte )
+{
+   if ( s_dataIndex < s_frame.length )
+   {
+      *_byte  = s_frame.data.bytes[s_dataIndex];
+      Serial.print((char) *_byte);
+      s_dataIndex++;
+      return SQ_TRUE;
+   }
+   else
+   {
+      return SQ_FALSE;
+   }
+}
+
+static void can0_send()
+{
+   size_t i;
+   for ( i = 0; i < s_sendFrame.length; i++ )
+   {
+      Serial.print((char) s_sendFrame.data.bytes[i]);
+   }
+   bool sent = false;
+   do
+   {
+      sent = Can0.sendFrame(s_sendFrame);
+   } while ( !sent );
 }
 
 SQBool sq_stream_write_string ( SQStream * _stream, const char * const _string )
 {
-   int i;
-   size_t stringIndex = 0;
    size_t length = strlen(_string);
-   while ( length > 0 )
+   for ( size_t i = 0; i < length; i++ )
    {
-      if ( length >= 8 )
+      sq_stream_write_byte(_stream, _string[i]);
+   }
+   /*
+   size_t i;
+   size_t stringIndex = 0;
+   size_t left = strlen(_string);
+   while ( left > 0 )
+   {
+      if ( left > 8 )
       {
          s_sendFrame.length = 8;
          for ( i = 0; i < 8; i++ )
@@ -88,20 +151,22 @@ SQBool sq_stream_write_string ( SQStream * _stream, const char * const _string )
             s_sendFrame.data.bytes[i] = _string[stringIndex];
             stringIndex++;
          }
-         Can0.sendFrame(s_sendFrame);
-         length -= 8;
+         can0_send();
+         left -= 8;
       }
       else
       {
-         s_sendFrame.length = length;
-         for ( i = 0; i < length; i++ )
+         s_sendFrame.length = left;
+         for ( i = 0; i < left; i++ )
          {
             s_sendFrame.data.bytes[i] = _string[stringIndex];
             stringIndex++;
          }
-         Can0.sendFrame(s_sendFrame);
+         can0_send();
+         left = 0;
       }
    }
+   */
    return SQ_TRUE;
 }
 
@@ -116,32 +181,31 @@ SQBool sq_stream_write_SQStringOut ( SQStream * _stream, SQStringOut *pString )
 
 SQBool sq_stream_write_byte ( SQStream * _stream, SQByte _byte )
 {
-   s_sendFrame.length = 1;
-   s_sendFrame.data.bytes[0] = _byte;
-   Can0.sendFrame(s_sendFrame);
+   s_sendFrame.data.bytes[s_sendIndex] = _byte;
+   s_sendIndex ++;
+   if ( s_sendIndex == 8 )
+   {
+      s_sendFrame.length = 8;
+      can0_send();
+      s_sendIndex = 0;
+   }
    return SQ_TRUE;
-}
-
-SQBool sq_stream_read_byte ( SQStream * _stream, SQByte * _byte )
-{
-   if ( s_dataIndex < s_frame.length )
-   {
-      *_byte  = s_frame.data.bytes[s_dataIndex];
-      s_dataIndex++;
-      return SQ_TRUE;
-   }
-   else
-   {
-      return SQ_FALSE;
-   }
 }
 
 void sq_stream_enter_write ( SQStream * _stream )
 {
+   Serial.print(" TX ");
+   s_sendIndex = 0;
 }
 
 void sq_stream_exit_write ( SQStream * _stream )
 {
+   if ( s_sendIndex > 0 )
+   {
+      s_sendFrame.length = s_sendIndex;
+      can0_send();
+      s_sendIndex = 0;
+   }
 }
 
 void sq_stream_join ( SQStream * _stream )
