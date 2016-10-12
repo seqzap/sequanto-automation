8f19a4cd6806b241795e3687ca7a674da1096f0f
Rasmus Toftdahl Olesen <github@halfdans.net>
2016-10-12 11:24:31 +0200

    Implement mouse button simulation using SendInput API on win32

--- server/c/applications/machine_automation/mouse_win32.c
+++ server/c/applications/machine_automation/mouse_win32.c
@@ -1,13 +1,91 @@
+#include <windows.h>
+#include <sequanto/types.h>
+
 #include "config.h"
 
-void mouse_click_at ( int _x, int _y, int _button )
+static DWORD mouse_get_mouse_event_flags(int _button, SQBool _up)
 {
+    switch (_button)
+    {
+    case 1:
+        switch (_up)
+        {
+        case SQ_TRUE:
+            return MOUSEEVENTF_RIGHTUP;
+
+        default:
+            return MOUSEEVENTF_RIGHTDOWN;
+        }
+
+    case 2:
+        switch (_up)
+        {
+        case SQ_TRUE:
+            return MOUSEEVENTF_MIDDLEUP;
+
+        default:
+            return MOUSEEVENTF_MIDDLEDOWN;
+        }
+
+    default:
+        switch (_up)
+        {
+        case SQ_TRUE:
+            return MOUSEEVENTF_LEFTUP;
+
+        default:
+            return MOUSEEVENTF_LEFTDOWN;
+        }
+    }
 }
 
-void mouse_hold_at ( int _x, int _y, int _button )
+void mouse_click_at(int _x, int _y, int _button)
 {
+    INPUT input;
+
+    SetCursorPos(_x, _y);
+
+    input.type = INPUT_MOUSE;
+    input.mi.dx = _x;
+    input.mi.dy = _y;
+    input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | mouse_get_mouse_event_flags(_button, SQ_FALSE);
+    input.mi.time = 0;
+    input.mi.mouseData = 0;
+    input.mi.dwExtraInfo = 0;
+    SendInput(1, &input, sizeof(INPUT));
+
+    input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | mouse_get_mouse_event_flags(_button, SQ_TRUE);
+    SendInput(1, &input, sizeof(INPUT));
 }
 
-void mouse_release_at ( int _x, int _y, int _button )
+void mouse_hold_at(int _x, int _y, int _button)
 {
+    INPUT input;
+
+    SetCursorPos(_x, _y);
+
+    input.type = INPUT_MOUSE;
+    input.mi.dx = _x;
+    input.mi.dy = _y;
+    input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | mouse_get_mouse_event_flags(_button, SQ_FALSE);
+    input.mi.time = 0;
+    input.mi.mouseData = 0;
+    input.mi.dwExtraInfo = 0;
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+void mouse_release_at(int _x, int _y, int _button)
+{
+    INPUT input;
+
+    SetCursorPos(_x, _y);
+
+    input.type = INPUT_MOUSE;
+    input.mi.dx = _x;
+    input.mi.dy = _y;
+    input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | mouse_get_mouse_event_flags(_button, SQ_TRUE);
+    input.mi.time = 0;
+    input.mi.mouseData = 0;
+    input.mi.dwExtraInfo = 0;
+    SendInput(1, &input, sizeof(INPUT));
 }
